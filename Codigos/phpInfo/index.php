850c96d7d176b58ba6293131acb46f75533db5d6
JOHNOVERDADEIROEUNICO <joaopedroteixeira26@gmail.com>
2026-01-09 08:58:21 -0300

    começando a codar em php
    tudo o que fiz!

---
+++ Codigos/phpInfo/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dados do servidor</title>
+</head>
+<body>
+    <h1>Dados do servidor</h1>
+
+    <?php 
+        phpinfo();/*Cria uma tabelaa gigante com varios dados da versão do php e outras informações sobre o servidor.*/
+    ?>
+</body>
+</html>
---
+++ Codigos/primeiro codigo/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Me livrando da maldição</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            echo"Olá, Mundo!";
+            print"Olá, Mundo2!";
+            /* Primeiro perceba que os comentários são indênticos aos do CSS, segundo, como vimos anteriormente na parte de explicações Rasmus desenvolveu a base do php com a vantagem de que ele pudesse progamar usando C e perl, perceba que o print é usado em C e o echo em perl para escrever algo na tela.*/
+        ?>
+        <!--Isto que vocÊ está vendo é o que chamamos de Super Tag. Antigamente se usava script com a função leanguage, hoje em dia, se utiliza esta tag.-->
+    </h1>
+    <p>Vamos tentar nos livrar da maldição.</p>
+</body>
+</html>
