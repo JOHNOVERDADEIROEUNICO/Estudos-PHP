124665bfbde3657bb934ca10a2a2fcebbcfbe9d9
JOHNOVERDADEIROEUNICO <joaopedroteixeira26@gmail.com>
2026-01-30 16:58:59 -0300

    Update lutadores.php

--- Codigos/POO em PHP/desafios/desafio2/lutadores.php
+++ Codigos/POO em PHP/desafios/desafio2/lutadores.php
@@ -61,44 +61,81 @@
             $this -> categoria = $var;
         }
         private function setVitorias($var){
-            $this -> vitorias = $var;
+            $this -> vitorias += $var;
         }
         private function setDerrotas($var){
-            $this -> derrotas = $var;
+            $this -> derrotas += $var;
         }
         private function setEmpates($var){
-            $this -> empates = $var;
+            $this -> empates += $var;
         }
 
         //Construtor
         public function __construct($n, $na, $i, $al, $pes, $cat, $vic, $loss, $draw)
         {
-            $this -> nome = $n;
-            $this -> nacionalidade = $na;
-            $this -> idade = $i;
-            $this -> altura = $al;
-            $this -> peso = $pes;
-            $this -> categoria = $cat;
-            $this -> vitorias = $vic;
-            $this -> derrotas = $loss;
-            $this -> empates = $draw;
+            $this -> setNome($n);
+            $this -> setNacionalidade($na);
+            $this -> setIdade($i);
+            $this -> setAltura($al);
+            $this -> setPeso($pes);
+            $this -> setCategoria($cat);
+            $this -> setVitorias($vic);
+            $this -> setDerrotas($loss);
+            $this -> setEmpates($draw);
         }
 
         //Metodos
         public function Apresentar(){
+            echo "<p>Ladies and gentlemen, we are... live! From the [Local do Evento]! This is the main event of the evening!</p>";
 
+            echo "<p>This is the moment you've all been waiting for! Live! IIIIIIIIIIIIII t's TIIIIIime!</p>";
+
+            echo "<p>Introducing first, fighting out of the blue/red corner... weighing {$this -> getPeso()} from {$this -> getCategoria()}category, with{$this -> getAltura()}</p>, from {$this -> getNacionalidade()} The One, The Only, {$this -> getNome()}";
         }
         public function Status(){
+            echo "<p>O lutador {$this -> getNome()} possui: </p>";
+
+            echo "<p>Vitorias: {$this -> getVitorias()}</p>";
+
+            echo "<p>Empates: {$this -> getEmpates()}</p>";
             
+            echo "<p>Derrotas: {$this -> getDerrotas()}</p>";
         }
         public function GanharLuta(){
-            
+            $this -> setVitorias(1);
         }
         public function PerderLuta(){
-            
+            $this -> setDerrotas(1);
         }
         public function EmpatarLuta(){
-            
+            $this -> setEmpates(1);
+        }
+        public function Lutar(object $l1, object $l2){
+            $quemVenceu = mt_rand(1, 3);
+
+            if  ($l1 -> getCategoria() === $l2 -> getCategoria() && $l1-> getPeso() === $l2-> getPeso()){
+                if ($quemVenceu == 1){
+                    $l1 -> GanharLuta();
+
+                    $l2 -> PerderLuta();
+                }
+                else if ($quemVenceu == 2){
+                    $l1 -> PerderLuta();
+
+                    $l2 -> GanharLuta();
+                }
+                else if ($quemVenceu == 3){
+                    $l1 -> EmpatarLuta();
+
+                    $l2 -> EmpatarLuta();
+                }
+            }
+            else if ($l1 -> getCategoria() === $l2 -> getCategoria() && $l1-> getPeso()  != $l2-> getPeso()){
+                echo "<p>Um dos lutadores não bateu o peso.</p>";
+            }
+            else if ($l1 -> getCategoria() != $l2 -> getCategoria()){
+                echo "<p>Os lutadores são de categorias diferentes.</p>";
+            }
         }
 
     }
